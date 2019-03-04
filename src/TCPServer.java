6ee2385474ec8c894afb1f8fc961287fc839838f
bernhard <bernhard@isys.uni-klu.ac.at>
2019-03-04 13:59:49 +0100

    new jar

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 
 public class TCPServer implements Runnable {
 
-    public static final int port = 12345;
+    public static final int port = 53212;
 
     public void run() {
         System.out.printf("Running new instance");
